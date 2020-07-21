0599d829ea3753ee0af5c00acf065883adb9861f
Maycie Morris <maycie@MacBook-Pro.local>
2020-07-21 12:06:27 -0700

    Completed task 2

--- index.js
+++ index.js
@@ -4,22 +4,41 @@ console.log(fifaData);
 console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+fifaData.indexOf(2014);
+
+/*(a) Home Team name for 2014 world cup final*/
+
+console.log(fifaData[828]["Home Team Name"])
+
+/*(b) Away Team name for 2014 world cup final*/
+
+console.log(fifaData[828]["Away Team Name"])
+
+/*(c) Home Team goals for 2014 world cup final*/
+
+console.log(fifaData[828]["Home Team Goals"])
+
+/*(d) Away Team goals for 2014 world cup final*/
+
+console.log(fifaData[828]["Away Team Goals"])
+
+/*(e) Winner of 2014 world cup final */
+
+console.log(fifaData[828]["Win conditions"])
+
+
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
+    
+    return fifaData.filter((game) => game.Stage === 'Final')
+}
 
-    /* code here */
-
-};
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
